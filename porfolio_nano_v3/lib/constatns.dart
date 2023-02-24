956eaf7ce6a2538ad92127cf7df55b141b832e04
NanoCode10 <marianoars10@gmail.com>
2023-02-24 09:11:01 -0300

    Version 1.0.3.2

--- porfolio_nano_v3/lib/constatns.dart
+++ porfolio_nano_v3/lib/constatns.dart
@@ -73,7 +73,7 @@ class _MyCustomNavigationRailState extends State<MyCustomNavigationRail> {
               });
             },
             children: [
-              homepage(),
+              homepage_dcktop(),
               aboutme(),
               skills(),
               contact(),
@@ -161,7 +161,7 @@ class _PageViwCustomState extends State<PageViwCustom> {
         });
       },
       children: [
-        homepage(),
+        homepage_mobile(),
         aboutme(),
         skills(),
         contact(),
--- porfolio_nano_v3/lib/pages/homePages.dart
+++ porfolio_nano_v3/lib/pages/homePages.dart
@@ -1,26 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
-class homepage extends StatelessWidget {
-  const homepage({Key? key}) : super(key: key);
+class homepage_dcktop extends StatelessWidget {
+  const homepage_dcktop({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return ListView(
       scrollDirection: Axis.horizontal,
       children: [
-        SizedBox(
+        Container(
+          //  child: SizedBox(
           child: Container(
-            height: 100,
-            width: 200,
-            margin: EdgeInsets.only(bottom: 10),
+            height: MediaQuery.of(context).size.height / 10,
+            width: MediaQuery.of(context).size.width / 3,
+            margin: EdgeInsets.only(bottom: 50, left: 90, right: 50, top: 50),
             color: Colors.deepOrange,
           ),
+          // ),
         ),
+        Container(
+          height: MediaQuery.of(context).size.height / 10,
+          width: MediaQuery.of(context).size.width / 2.3,
+          margin: EdgeInsets.only(bottom: 50, left: 50, right: 50, top: 50),
+          color: Colors.green,
+        ),
+        //SizedBox(
+
+        SizedBox(
+          width: 500.0,
+          height: 500.0,
+          child: ListView(
+            scrollDirection: Axis.vertical,
+            children: [
+              Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height,
+                margin:
+                    EdgeInsets.only(bottom: 50, left: 50, right: 50, top: 50),
+                color: Colors.deepPurpleAccent,
+              ),
+            ],
+          ),
+        ),
+
+        //   )
       ],
     );
 
     //return Container(
     //  color: Color.fromARGB(255, 241, 236, 236),
     //);
+  }
+}
+
+class homepage_mobile extends StatelessWidget {
+  const homepage_mobile({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Color.fromARGB(255, 241, 236, 236),
+    );
   }
 }
